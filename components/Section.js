4b75aa92026c6b582337b055b786b0fed2602e9c
= <timorolf05@gmail.com>
2022-08-05 23:02:24 +0200

    viewable on mobile now

--- components/Section.js
+++ components/Section.js
@@ -1,7 +1,35 @@
+import { useEffect, useRef } from "react";
 import styles from "./section.module.css";
 
 function Section(props) {
-  return <section id={styles.section}>{props.children}</section>;
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    if (!sectionRef?.current) return;
+
+    const resizeHandler = () => {
+      const { height, width } = sectionRef.current.getBoundingClientRect();
+
+      const scale = Math.min(
+        (0.5 * window.innerWidth) / width,
+        (0.5 * window.innerHeight) / height,
+        1
+      );
+      console.log(scale);
+
+      sectionRef.current.style.transform = `scale(${scale})`;
+    };
+
+    window.addEventListener("resize", resizeHandler);
+    resizeHandler();
+    return () => window.removeEventListener("resize", resizeHandler);
+  }, [sectionRef]);
+
+  return (
+    <section ref={sectionRef} id={styles.section}>
+      {props.children}
+    </section>
+  );
 }
 
 export default Section;
